cb842a14ff473232f4f6085aab4177991a2562c5
michalstechly <michal.stechly@gmail.com>
2020-12-11 19:08:16 -0500

    Fixed positional parameters in graph.

--- steps/graph.py
+++ steps/graph.py
@@ -19,7 +19,12 @@ def generate_random_graph_erdos_renyi(
     if seed == "None":
         seed = None
     graph = _generate_random_graph_erdos_renyi(
-        number_of_nodes, edge_probability, random_weights, seed
+        number_of_nodes,
+        edge_probability,
+        random_weights,
+        min_weight=min_weight,
+        max_weight=max_weight,
+        seed=seed,
     )
     save_graph(graph, "graph.json")
 
@@ -35,22 +40,32 @@ def generate_random_regular_graph(
     if seed == "None":
         seed = None
     graph = _generate_random_regular_graph(
-        number_of_nodes, degree, random_weights, seed
+        number_of_nodes,
+        degree,
+        random_weights,
+        min_weight=min_weight,
+        max_weight=max_weight,
+        seed=seed,
     )
     save_graph(graph, "graph.json")
 
 
 def generate_complete_graph(
-    number_of_nodes: int, 
-    random_weights: bool = False, 
+    number_of_nodes: int,
+    random_weights: bool = False,
     min_weight: int = 0,
     max_weight: int = 1,
-    seed: Union[str, int] = "None"
+    seed: Union[str, int] = "None",
 ):
     if seed == "None":
         seed = None
     graph = _generate_random_graph_erdos_renyi(
-        number_of_nodes, 1.0, random_weights, seed
+        number_of_nodes,
+        1.0,
+        random_weights,
+        min_weight=min_weight,
+        max_weight=max_weight,
+        seed=seed,
     )
     save_graph(graph, "graph.json")
 
