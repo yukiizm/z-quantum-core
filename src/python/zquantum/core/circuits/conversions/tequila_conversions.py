b1955f7fbbc5c12fa8b74540ec77cf4a46795320
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-06-23 12:03:48 +0200

    Add support for exporting single qubit gates

--- src/python/zquantum/core/circuits/conversions/tequila_conversions.py
+++ src/python/zquantum/core/circuits/conversions/tequila_conversions.py
@@ -2,12 +2,29 @@ import operator
 from functools import reduce
 
 import tequila as tq
+import tequila.circuit.gates
 
 from .. import _circuit, _gates, _operations
 
 
 def import_from_tequila(circuit: tq.QCircuit) -> _circuit.Circuit:
     pass
+
+
+# TODO: figure out how to handle I gate
+
+ZQUANTUM_NAME_TO_TEQUILA_GATE = {
+    name: getattr(tq.circuit.gates, name)
+    for name in [
+        "X",
+        "Y",
+        "Z",
+        "H",
+        # "I",
+        "S",
+        "T",
+    ]
+}
 
 
 def export_to_tequila(circuit: _circuit.Circuit) -> tq.QCircuit:
@@ -19,8 +36,5 @@ def _export_operation(operation: _operations.Operation) -> tq.QCircuit:
     if not isinstance(operation, _gates.GateOperation):
         raise TypeError(f"Can't export {type(operation)} to tequila")
 
-    op: _gates.GateOperation = operation
-    if op.gate.name == "X":
-        return tq.gates.X(op.qubit_indices[0])
-
-    raise NotImplementedError()
+    gate_factory = ZQUANTUM_NAME_TO_TEQUILA_GATE[operation.gate.name]
+    return gate_factory(*operation.qubit_indices)
--- tests/zquantum/core/circuits/conversions/tequila_conversions_test.py
+++ tests/zquantum/core/circuits/conversions/tequila_conversions_test.py
@@ -1,10 +1,33 @@
 import pytest
 import tequila as tq
+import tequila.circuit.gates
 from zquantum.core import circuits
 from zquantum.core.circuits.conversions import tequila_conversions
 
 EQUIVALENT_CIRCUITS = [
     (circuits.Circuit([]), tq.circuit.QCircuit()),
+    (circuits.Circuit([circuits.X(0)]), tq.circuit.gates.X(0)),
+    (
+        circuits.Circuit(
+            [
+                circuits.X(1),
+                circuits.Y(0),
+                circuits.Z(4),
+                circuits.H(3),
+                # circuits.I(1),
+                circuits.S(1),
+                circuits.T(1),
+            ]
+        ),
+        (
+            tq.gates.X(1)
+            + tq.circuit.gates.Y(0)
+            + tq.circuit.gates.Z(4)
+            + tq.circuit.gates.H(3)
+            + tq.circuit.gates.S(1)
+            + tq.circuit.gates.T(1)
+        ),
+    ),
 ]
 
 
