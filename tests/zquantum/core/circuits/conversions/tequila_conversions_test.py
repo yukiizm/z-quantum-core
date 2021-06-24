c355246ff960b2a826df28198bbbcd21adcfe7fd
Alexander Juda <alexander.juda@zapatacomputing.com>
2021-06-24 13:28:56 +0200

    Split circuit equivalence tests to exact match and close match

--- tests/zquantum/core/circuits/conversions/tequila_conversions_test.py
+++ tests/zquantum/core/circuits/conversions/tequila_conversions_test.py
@@ -6,12 +6,17 @@ import tequila.circuit.gates
 from zquantum.core import circuits
 from zquantum.core.circuits.conversions import tequila_conversions
 
-EQUIVALENT_CIRCUITS = [
+# Example circuits that contain gates defined natively in both ZQuantum and Tequila,
+# meaning:
+# - the gate has an explicit named constructor
+# - the gate has the same name as in the other framework
+# - the gate has the same matrix as in the other framework (TODO: validate it)
+NATIVE_GATES_EQUIVALENT_CIRCUITS = [
     # empty
     (circuits.Circuit([]), tq.circuit.QCircuit()),
     # simplest case - single gate circuit
     (circuits.Circuit([circuits.X(0)]), tq.circuit.gates.X(0)),
-    # all ZQuantum single-qubit, non-parametric gates
+    # --- single-qubit, non-parametric gates ---
     (
         circuits.Circuit(
             [
@@ -19,8 +24,6 @@ EQUIVALENT_CIRCUITS = [
                 circuits.Y(0),
                 circuits.Z(4),
                 circuits.H(3),
-                circuits.S(1),
-                circuits.T(1),
             ]
         ),
         (
@@ -28,11 +31,9 @@ EQUIVALENT_CIRCUITS = [
             + tq.circuit.gates.Y(0)
             + tq.circuit.gates.Z(4)
             + tq.circuit.gates.H(3)
-            + tq.circuit.gates.S(1)
-            + tq.circuit.gates.T(1)
         ),
     ),
-    # all ZQuantum single-qubit, parametric gates
+    # --- single-qubit, parametric gates ---
     *[
         (
             circuits.Circuit(
@@ -41,7 +42,6 @@ EQUIVALENT_CIRCUITS = [
                     circuits.RY(angle)(0),
                     circuits.RZ(angle)(4),
                     circuits.PHASE(angle)(1),
-                    circuits.U3(angle, angle + 0.1, angle - 0.3)(0),
                 ]
             ),
             (
@@ -49,27 +49,88 @@ EQUIVALENT_CIRCUITS = [
                 + tq.circuit.gates.Ry(angle, 0)
                 + tq.circuit.gates.Rz(angle, 4)
                 + tq.circuit.gates.Phase(1, None, angle)
-                + tq.circuit.gates.u3(angle, angle + 0.1, angle - 0.3, 0)
             ),
         )
         for angle in [0.0, 0.1, np.pi, 2 * np.pi, np.pi / 5, 10 * np.pi]
     ],
-    # all ZQuantum two-qubit, non-parametric gates
+    # --- two-qubit, non-parametric gates ---
+    (
+        circuits.Circuit(
+            [
+                circuits.SWAP(3, 1),
+            ]
+        ),
+        tq.circuit.gates.SWAP(3, 1),
+    ),
+]
+
+
+def _normalize_gate_name(name: str):
+    return name.lower()
+
+
+@pytest.mark.parametrize(
+    "zquantum_circuit,tequila_circuit", NATIVE_GATES_EQUIVALENT_CIRCUITS
+)
+class TestCircuitEquivalenceForNativeGates:
+    class TestFixtureHealth:
+        def test_gate_names_match(self, zquantum_circuit, tequila_circuit):
+            for zquantum_op, tequila_gate in zip(
+                zquantum_circuit.operations, tequila_circuit.gates
+            ):
+                assert _normalize_gate_name(
+                    zquantum_op.gate.name
+                ) == _normalize_gate_name(tequila_gate.name)
+
+    class TestExporting:
+        def test_matches_equivalent_circuit(self, zquantum_circuit, tequila_circuit):
+            assert (
+                tequila_conversions.export_to_tequila(zquantum_circuit)
+                == tequila_circuit
+            )
+
+
+# Gates that are similar between ZQuantum and Tequila, but contain some implicit
+# processing.
+NON_NATIVE_EQUIVALENT_CIRCUITS = [
+    # --- single-qubit, non-parametric gates ---
+    # S and T gates are implicitly converted to phase by Tequila
+    (
+        circuits.Circuit(
+            [
+                circuits.S(2),
+                circuits.T(1),
+            ]
+        ),
+        tq.circuit.gates.S(2) + tq.circuit.gates.T(1),
+    ),
+    # --- single-qubit, parametric gates ---
+    # u3 is implicitly decomposed to a circuit with RX+RY+RZ gates
+    *[
+        (
+            circuits.Circuit(
+                [
+                    circuits.U3(angle, angle + 0.1, angle - 0.3)(0),
+                ]
+            ),
+            tq.circuit.gates.u3(angle, angle + 0.1, angle - 0.3, 0),
+        )
+        for angle in [0.0, 0.1, np.pi, 2 * np.pi, np.pi / 5, 10 * np.pi]
+    ],
+    # --- two-qubit, non-parametric gates ---
+    # CNOT is implicitly translated to X with control argument
+    # CZ is implicitly translated to Z with control argument
     (
         circuits.Circuit(
             [
                 circuits.CNOT(0, 1),
                 circuits.CZ(0, 3),
-                circuits.SWAP(3, 1),
             ]
         ),
-        (
-            tq.gates.CNOT(control=0, target=1)
-            + tq.circuit.gates.CZ(control=0, target=3)
-            + tq.circuit.gates.SWAP(3, 1)
-        ),
+        tq.gates.CNOT(control=0, target=1) + tq.circuit.gates.CZ(control=0, target=3),
     ),
-    # all ZQuantum two-qubit, parametric gates
+    # --- two-qubit, parametric gates ---
+    # Tequila has no explicit CPHASE gate
     *[
         (
             circuits.Circuit(
@@ -77,20 +138,20 @@ EQUIVALENT_CIRCUITS = [
                     circuits.CPHASE(angle)(1, 3),
                 ]
             ),
-            (tq.circuit.gates.Phase(target=3, control=1, angle=angle)),
+            tq.circuit.gates.Phase(target=3, control=1, angle=angle),
         )
         for angle in [0.0, 0.1, np.pi, 2 * np.pi, np.pi / 5, 10 * np.pi]
     ],
 ]
 
 
-class TestCircuitEquivalence:
-    @pytest.mark.parametrize("zquantum_circuit,tequila_circuit", EQUIVALENT_CIRCUITS)
-    def test_exporting_matches_equivalent_circuit(
-        self, zquantum_circuit, tequila_circuit
-    ):
-        assert (
-            tequila_conversions.export_to_tequila(zquantum_circuit) == tequila_circuit
-        )
-
-    # TODO: compare matrices after the export
+@pytest.mark.parametrize(
+    "zquantum_circuit,tequila_circuit", NON_NATIVE_EQUIVALENT_CIRCUITS
+)
+class TestCircuitEquivalenceForNonNativeGates:
+    class TestExporting:
+        def test_matches_equivalent_circuit(self, zquantum_circuit, tequila_circuit):
+            assert (
+                tequila_conversions.export_to_tequila(zquantum_circuit)
+                == tequila_circuit
+            )
